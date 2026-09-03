8c599c61873871f734af84f21bfab94d39bb8797
Md. Ibna Roufe Prio <prioir22@gmail.com>
2026-09-03 22:38:11 +0600

    Update ProductController.java

--- src/main/java/com/iecommerce/ecommerce/controller/ProductController.java
+++ src/main/java/com/iecommerce/ecommerce/controller/ProductController.java
@@ -1,6 +1,5 @@
 package com.iecommerce.ecommerce.controller;
 
-
 import com.iecommerce.ecommerce.entity.Product;
 import com.iecommerce.ecommerce.service.CategoryService;
 import com.iecommerce.ecommerce.service.ProductService;
@@ -8,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequiredArgsConstructor
@@ -28,19 +28,35 @@ public class ProductController {
     }
 
     @PostMapping("/save")
-    public String saveProduct(@ModelAttribute Product product) {
+    public String saveProduct(
+            @ModelAttribute Product product,
+            @RequestParam(value = "imageFile", required = false) MultipartFile imageFile,
+            @RequestParam(value = "imageUrl", required = false) String imageUrl) {
 
-        productService.saveProduct(product);
+        productService.saveProduct(product, imageFile, imageUrl);
 
         return "redirect:/admin/products";
     }
 
     @GetMapping("/edit/{id}")
-    public String editProduct(@PathVariable Long id, Model model) {
+    public String editProduct(
+            @PathVariable Long id,
+            Model model) {
 
-        model.addAttribute("product", productService.getProductById(id));
-        model.addAttribute("products", productService.getAllProducts());
-        model.addAttribute("categories", categoryService.getAllCategories());
+        model.addAttribute(
+                "product",
+                productService.getProductById(id)
+        );
+
+        model.addAttribute(
+                "products",
+                productService.getAllProducts()
+        );
+
+        model.addAttribute(
+                "categories",
+                categoryService.getAllCategories()
+        );
 
         return "admin/products";
     }
@@ -52,6 +68,4 @@ public class ProductController {
 
         return "redirect:/admin/products";
     }
-
-
 }
